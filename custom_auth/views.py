a9265d4ebdb02c9db54c470ef8453c1004b1df19
Yersultan <104057714+Yyerss@users.noreply.github.com>
2023-11-27 14:37:07 +0600

    register view with generics

--- custom_auth/views.py
+++ custom_auth/views.py
@@ -4,17 +4,36 @@ from rest_framework import status
 from rest_framework.parsers import JSONParser, MultiPartParser, FormParser
 from .models import UserProfile
 from .serializers import UserProfileSerializer
+from rest_framework import generics
+from rest_framework import mixins
 
 
-class RegisterUserView(APIView):
+# class RegisterUserView(APIView):
+#     parser_classes = [JSONParser, MultiPartParser, FormParser]
+#
+#     def post(self, request):
+#         if UserProfile.objects.filter(email=request.data['email']).exists():
+#             return Response({'error': 'Email already registered'}, status=status.HTTP_400_BAD_REQUEST)
+#         else:
+#             serializer = UserProfileSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class RegisterUserView(generics.GenericAPIView, mixins.CreateModelMixin):
     parser_classes = [JSONParser, MultiPartParser, FormParser]
+    serializer_class = UserProfileSerializer
 
-    def post(self, request):
-        if UserProfile.objects.filter(email=request.data['email']).exists():
+    def post(self, request, *args, **kwargs):
+        if UserProfile.objects.filter(email=request.data.get('email')).exists():
             return Response({'error': 'Email already registered'}, status=status.HTTP_400_BAD_REQUEST)
         else:
-            serializer = UserProfileSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return self.create(request, *args, **kwargs)
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+    def get_success_headers(self, data):
+        return {'Location': str(data['id'])}
