933cc46e30b6c2dcfe95fcab968d837c33e9f978
Mukagali <mukagaly.omirzak2003@gmail.com>
2023-12-18 17:56:03 +0600

    correct url brand

--- brand/urls.py
+++ brand/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("/brands", views.BrandListView.as_view()),
-    path("/brands/<int:pk>", views.BrandDetailView.as_view()),
+    path("brands/", views.BrandListView.as_view()),
+    path("brands/<int:pk>", views.BrandDetailView.as_view()),
 ]
