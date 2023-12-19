3cd6223878a83d2c548e0053ac9add080d6253d8
Yersultan <104057714+Yyerss@users.noreply.github.com>
2023-12-19 23:46:14 +0600

    Brand with name in url ignore case

--- brand/views.py
+++ brand/views.py
@@ -29,4 +29,9 @@ class BrandDetailView(mixins.RetrieveModelMixin, GenericAPIView):
     lookup_field = 'name'
 
     def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
+        filter_kwargs = {'name__iexact': kwargs['name']}
+        brand = get_object_or_404(Brand, **filter_kwargs)
+        serializer = self.get_serializer(brand)
+        return Response(serializer.data)
+
+
