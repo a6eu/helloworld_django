ff38d181f0452575e89af6814bd8d6719df6e0e5
Mukagali <mukagaly.omirzak2003@gmail.com>
2024-02-08 21:50:22 +0600

    port change

--- launch.sh
+++ launch.sh
@@ -6,4 +6,4 @@ python manage.py collectstatic --noinput
 echo 'Applying migrations...'
 python manage.py migrate
 
-gunicorn site_market.wsgi:application --bind 0.0.0.0:$PORT
+gunicorn site_market.wsgi:application --bind 0.0.0.0:9000
