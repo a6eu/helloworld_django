657ae75f068d794c05fb6929c2335493d6129768
Mukagali <119991483+MukagaliSDU@users.noreply.github.com>
2024-03-21 23:12:24 +0500

    Update tasks.py

--- products/tasks.py
+++ products/tasks.py
@@ -12,31 +12,31 @@ import xml.etree.ElementTree as ET
 
 @app.task()
 def update_products():
-    # url = "https://b2b.marvel.kz/Api/GetFullStock"
-    # params = {
-    #     "user": "01itgroup04",
-    #     "password": "m_u_oOV8N1",
-    #     "secretKey": "",
-    #     "packStatus": 0,
-    #     "responseFormat": 1,
-    #     "instock": 0,
-    #     "updatedSince": "24072019"
-    # }
-    # response = requests.post(url, params=params)
-    # if response.status_code == 200:
-    #     try:
-    #         data = response.json()
-    #     except ValueError:
-    #         data = None
+    url = "https://b2b.marvel.kz/Api/GetFullStock"
+    params = {
+        "user": "01itgroup04",
+        "password": "m_u_oOV8N1",
+        "secretKey": "",
+        "packStatus": 0,
+        "responseFormat": 1,
+        "instock": 0,
+        "updatedSince": "24072019"
+    }
+    response = requests.post(url, params=params)
+    if response.status_code == 200:
+        try:
+            data = response.json()
+        except ValueError:
+            data = None
     json_file_path = '~/Downloads/Telegram Desktop/responseStock.json'
     json_file_path = os.path.expanduser(json_file_path)
 
-    try:
-        with open(json_file_path, 'r') as file:
-            data = json.load(file)
-    except FileNotFoundError:
-        print(f"Файл не найден: {json_file_path}")
-        return
+    # try:
+    #     with open(json_file_path, 'r') as file:
+    #         data = json.load(file)
+    # except FileNotFoundError:
+    #     print(f"Файл не найден: {json_file_path}")
+    #     return
 
     if data and 'Body' in data and 'CategoryItem' in data['Body']:
         products_data = data['Body']['CategoryItem']
